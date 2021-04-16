e06c592bbf419fecb2e2b9f627acfa4ec4617694
basedmilz <basedmilz@gmail.com>
2021-04-16 09:02:00 -0400

    update files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,40 @@
 // The following API routes should be created:
-const notesData = require('../db/db.json');
+var notesData = require('../db/db.json');
 const fs = require('fs');
 const express = require('express');
-const uniqId = require('uniqid');
-// const { allowedNodeEnvironmentFlags } = require('process');
-module.exports = (app) => { app.get('/api/notes', (req, res) => {res.json(notesData)})
+
+module.exports = (app) => {
+     app.get('/api/notes', (req, res) => {res.json(notesData)})
     // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 
-    app.get('/api/notes', (req, res) => res.json(notesData));
+    
     // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
     // Help from classmates to find a solution
     app.post('/api/notes', (req, res) => {
         const newNote = {
             title: req.body.title,
             text: req.body.text,
-            id: uniqId,
+            id: notesData.length,
         };
-        notesData.push(req.body)
-        const notes = JSON.parse(fs.readFileSync('../db/db.json'));
-        newNote.id = (notes.length).string();
-        fs.writeFileSync('/db/db.json', JSON.stringify(noteArray));
+        console.log('this is new note', newNote)
+        notesData.push(newNote);
+        console.log('this is the notes data', notesData )
+        
+        fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
         res.sendStatus(200);
       
     });
-    app.get('/api/notes', (req, res) => {
-        const notes = fs.readFileSync('./db/db.json', 'utf8')
-        res.json(JSON.parse(notes))
-    })
+    
 // Get all notes
-    app.get("/api/notes", (req, res) => {
-        res.json(notesData);
-    })
+    app.delete("/api/notes/:id", (req, res) => {
+       console.log(req.params)
+       var newArray = notesData.filter(note=> note.id !== parseInt(req.params.id))
+       console.log(newArray)
+       notesData = newArray
+       fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+        res.sendStatus(200);
+       
+})
 
    
 }
